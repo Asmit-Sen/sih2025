f02ce5a12edc3c954ec4ea1cbaa503dd5277cdc4
Asmit Sen <susmita.sen1130@gmail.com>
2025-09-13 21:37:06 +0530

    v0 - init proj

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+const app= express()    
+const PORT= 8000
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/test',(req,res)=>{
+    res.json({
+        "msg":"test running"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
